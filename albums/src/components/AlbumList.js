b09d2fd19b3a5ceb44136dadf8b03107b4526099
Anish Surti <srtanish1992@gmail.com>
2018-03-02 14:28:23 -0600

    added Header

---
+++ albums/src/components/AlbumList.js
@@ -0,0 +1,26 @@
+import React, { Component } from 'react';
+import { View, Text } from 'react-native';
+import axios from 'axios';
+
+class AlbumList extends Component {
+
+  state = { albums: [] };
+
+  // lifecycle method
+  componentWillMount() {
+    axios.get('https://rallycoding.herokuapp.com/api/music_albums')
+      .then(response => this.setState({ albums: response.data }));
+  }
+
+  render() {
+    console.log(this.state);
+
+    return (
+      <View>
+        <Text>AlbumList!</Text>
+      </View>
+    );
+  }
+}
+
+export default AlbumList;
